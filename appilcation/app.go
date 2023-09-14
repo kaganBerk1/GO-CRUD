27ae04a0a4a6a39bf56f57efcef446420dbf6a1b
kaganBerk1 <kaan_berk_gencer@hotmail.com>
2023-09-15 00:19:50 +0300

    Added routes

---
+++ appilcation/app.go
@@ -0,0 +1,28 @@
+package application
+import (
+	"net/http"
+	"fmt"
+	"context"
+)
+type App struct{
+	router http.Handler
+}
+
+func New() *App{
+	app:=&App{
+		router:loadRoutes()
+	}
+	return app
+}
+
+func (a *App) start(ctx context.Context) error {
+	server:=&http.Server{
+		Addr: ":3000",
+		Handler: a.router,
+	}
+	err:= server.ListenAndServe()
+	if err != nil{
+		return fmt.Errorf("fail to listen the server SADGE: %w", err)
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -1,28 +1,14 @@
 package main
 import (
 	"fmt"
-	"net/http"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"context"
+	"https://github.com/kaganBerk1/GO-CRUD/application"
 	)
 
 func main(){
-	fmt.Println("Hello, World!")
-
-	router:= chi.NewRouter()
-	router.Use(middleware.Logger)
-	router.Get("/hello",basicHandler)
-
-	server:=&http.Server{
-		Addr: ":3000",
-		Handler: router,
-	}
-	err:= server.ListenAndServe()
+	app:= application.New()
+	err:= app.Start()
 	if err != nil{
-		fmt.Println("fail to listen the server SADGE", err)
+		return fmt.Println("fail to listen the server SADGE: ")
 	}
-}
-
-func basicHandler(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("Hellow World From Server!"))
 }
