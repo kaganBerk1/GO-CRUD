003f8d8405578b8bba1176b78627f8139de26e8e
kaganBerk1 <kaan_berk_gencer@hotmail.com>
2023-09-25 23:27:55 +0300

    Data model For order added

--- appilcation/app.go
+++ appilcation/app.go
@@ -29,10 +29,13 @@ func (a *App) Start(ctx context.Context) error {
 	}
 	ch := make(chan error, 1)
 	err := a.rdb.Ping(ctx).Err()
+	if err != nil {
+		return fmt.Errorf("fail to 	conenct to redis: %w", err)
+	}
 	go func() {
 		err = server.ListenAndServe()
 		if err != nil {
-			ch <- fmt.Errorf("fail to 	conenct to redis: %w", err)
+			ch <- fmt.Errorf("fail to 	starting server %w", err)
 		}
 		close(ch)
 	}()
